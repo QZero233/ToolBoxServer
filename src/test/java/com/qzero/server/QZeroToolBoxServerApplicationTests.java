87872f52dead2c4cd39b86f77c0f11992f667f94
QZero <2540384328@qq.com>
2022-03-25 20:54:50 +0800

    Initialize

---
+++ src/test/java/com/qzero/server/QZeroToolBoxServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qzero.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QZeroToolBoxServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
